f18bcc05f1cedd66776b62336d1521ef800a3603
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2019-05-02 13:21:21 +0800

    Grab subpage links

--- webcrawler-program.py
+++ webcrawler-program.py
@@ -5,10 +5,17 @@ session = HTMLSession()
 
 r = session.get('https://careers.karma.life/people')
 
-guys_info = r.html.find(
-    '.thumbnail-hover.circle .img.u-tertiary-background-color')
+domain = 'https://careers.karma.life'
+
+guys_info = r.html.find('.thumbnail-hover.circle')
 
 
 for guy_info in guys_info:
+    absolute_link = domain + guy_info.attrs['href']
+    print(absolute_link)
+
+    '''
     urllib.request.urlretrieve(guy_info.attrs['data-src'], 'practice.jpg')
     break
+
+'''
