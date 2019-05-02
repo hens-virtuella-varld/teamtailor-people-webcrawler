e7770e58fc01694d94c050b69de5bb2356a27475
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2019-05-02 15:31:13 +0800

    Grab profile image, prograss in grabbing mail

--- webcrawler-program.py
+++ webcrawler-program.py
@@ -1,3 +1,4 @@
+import os
 import urllib.request
 from requests_html import HTMLSession
 
@@ -10,12 +11,35 @@ domain = 'https://careers.karma.life'
 guys_info = r.html.find('.thumbnail-hover.circle')
 
 
-for guy_info in guys_info:
-    absolute_link = domain + guy_info.attrs['href']
-    print(absolute_link)
+def grab_mail():
+    for guy_info in guys_info:
+        name = guy_info.find('.name', first=True).text
+        link = domain + guy_info.attrs['href']
+        r = session.get(link)
+        department_and_title = r.html.find('.info', first=True)
+        department =
+        title = r.html.find('.info', first=True)
+        mail = []
+        for contact_info in r.html.find('.contact-info a'):
+            string = contact_info.attrs['href']
+            if string.startswith('mailto:'):
+                mail.append(string.replace('mailto:', ''))
+        print(mail)
+        break
 
-    '''
-    urllib.request.urlretrieve(guy_info.attrs['data-src'], 'practice.jpg')
-    break
 
-'''
+def grab_profile_image():
+    if not os.path.exists("./karma/"):
+        os.mkdir('./karma')
+    for guy_info in guys_info:
+        name = guy_info.find('.name', first=True).text
+        link = domain + guy_info.attrs['href']
+        r = session.get(link)
+        image_element = r.html.find('.lazy.u-tertiary-background-color',
+                                    first=True)
+        if 'data-src' in image_element.attrs:
+            urllib.request.urlretrieve(
+                image_element.attrs['data-src'], './karma/' + name + '.jpg')
+
+
+grab_mail()
